b473ad22567ccbcf4e390b013177cbe364a9c995
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-01-20 01:34:20 +0530

    content multi step 1 created

--- src/page/content/index.jsx
+++ src/page/content/index.jsx
@@ -7,33 +7,43 @@ import {
   MenuOutlined,
 } from "@mui/icons-material";
 import CardView from "../../component/contentScreen/CardView";
+import CreateContent from "../../component/contentScreen/CreateContent";
 // import ListView from "../../component/contentScreen/ListView";
 
 const Content = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <div className="">
-      <div className=" flex w-full relative justify-between mob:gap-[10px] mob:flex-col mob:px-[10px] pl-[80px] pt-[30px] pr-[160px]">
-        <div className="flex items-center gap-3">
-          <Text className="text-[24px] mob:text-[16px] font-bold">Content</Text>
-          <MenuOutlined className="text-[#45818E]" />
+    <>
+      <div className="">
+        <div className=" flex w-full relative justify-between mob:gap-[10px] mob:flex-col mob:px-[10px] pl-[80px] pt-[30px] pr-[160px]">
+          <div className="flex items-center gap-3">
+            <Text className="text-[24px] mob:text-[16px] font-bold">
+              Content
+            </Text>
+            <MenuOutlined className="text-[#45818E]" />
+          </div>
+          <div className="flex text-[#45818E] items-center">
+            <Text className="pr-2 border-r-2 border-black">View List</Text>
+            <Text className="pr-2 ">
+              <CalendarMonth /> View Calender
+            </Text>
+          </div>
+          <div className="flex items-center mob:absolute mob:right-[10px] mob:bottom-0 mob:ml-auto">
+            <FilterAlt className="text-[#45818E]" />
+            <label onClick={handleOpen} className="flex px-3 py-2 cursor-pointer rounded-lg bg-[#45818E] text-white w-fit">
+              <Add className="!text-white" />
+              Add
+            </label>
+          </div>
         </div>
-        <div className="flex text-[#45818E] items-center">
-          <Text className="pr-2 border-r-2 border-black">View List</Text>
-          <Text className="pr-2 ">
-            <CalendarMonth /> View Calender
-          </Text>
-        </div>
-        <div className="flex items-center mob:absolute mob:right-[10px] mob:bottom-0 mob:ml-auto">
-          <FilterAlt className="text-[#45818E]" />
-          <label className="flex px-3 py-2 cursor-pointer rounded-lg bg-[#45818E] text-white w-fit">
-            <Add className="!text-white" />
-            Add
-          </label>
-        </div>
+        <CardView />
+        {/* <ListView /> */}
       </div>
-      <CardView />
-      {/* <ListView /> */}
-    </div>
+      <CreateContent open={open} handleClose={handleClose} />
+    </>
   );
 };
 
