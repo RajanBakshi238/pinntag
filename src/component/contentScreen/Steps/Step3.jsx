7d40bddb0c1a3b1c03ad0fc0f25963b0dc54c2e9
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-01-24 00:28:47 +0530

    map of create content intialized

--- src/component/contentScreen/Steps/Step3.jsx
+++ src/component/contentScreen/Steps/Step3.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const Step3 = () => {
   return (
-    <div>Step3</div>
-  )
-}
+    <div className="">
+      <div className="m-auto p-5 bg-[#DDDDD7] rounded-lg">
+        <MapContainer
+          style={{
+            height: "300px ",
+            width: "100%",
+            margin: "auto"
+          }}
+          center={[30.704649, 76.717873]}
+          zoom={15}
+        >
+          {/* add google map tile url  */}
+          <TileLayer
+            attribution="Google Maps"
+            url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
+          />
+        </MapContainer>
+      </div>
+    </div>
+  );
+};
 
-export default Step3
+export default Step3;
