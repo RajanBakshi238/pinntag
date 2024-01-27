785ecfa95f9b70f794ea3b89d81390b18172c9df
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-01-27 13:18:03 +0530

    model height and bottom bar ui fixed

--- src/common/Modal/PrimaryModal.jsx
+++ src/common/Modal/PrimaryModal.jsx
@@ -10,9 +10,14 @@ const PrimaryModal = ({ open, handleClose, children, modalClass }) => {
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <div className={classNames(["w-1/2 h-[90vh] overflow-auto py-2 px-5 outline-none bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-none rounded-lg", modalClass])}>
-        {children}
-    </div>
+      <div
+        className={classNames([
+          "w-1/2 h-[90vh] pl-5 outline-none bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-none rounded-lg",
+          modalClass,
+        ])}
+      >
+        <div className="py-2 pr-5 overflow-auto h-full">{children}</div>
+      </div>
     </Modal>
   );
 };
--- src/component/contentScreen/CreateContent.jsx
+++ src/component/contentScreen/CreateContent.jsx
@@ -4,71 +4,78 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import PrimaryModal from "../../common/Modal/PrimaryModal";
 import StepWizard from "./StepWizard";
 
-import Step1 from "./Steps/Step1"
+import Step1 from "./Steps/Step1";
 import Step2 from "./Steps/Step2";
 import SecondaryButton from "../../common/FormElements/Button/SecondaryButton";
 import PrimaryButton from "../../common/FormElements/Button/PrimaryButton";
 import Step3 from "./Steps/Step3";
 import Step4 from "./Steps/Step4";
 
-import "./content.css"
+import "./content.css";
 
-const INC = "inc"
-const DEC = "dec"
+const INC = "inc";
+const DEC = "dec";
 
 const CreateContent = ({ open, handleClose }) => {
   const [currentStep, setCurrentStep] = useState(1);
 
   const handleStep = (type) => {
-    if(currentStep === 4 && type === INC){
-      return
+    if (currentStep === 4 && type === INC) {
+      return;
     }
     if (type === INC) {
-      setCurrentStep((prev) => prev + 1)
+      setCurrentStep((prev) => prev + 1);
     } else if (type === DEC) {
-      setCurrentStep((prev) => prev - 1)
+      setCurrentStep((prev) => prev - 1);
     }
-  }
+  };
 
   return (
-    <PrimaryModal open={open} handleClose={handleClose} modalClass="w-[380px] sm:w-[500px] md:w-1/2 lg:w-2/5 xl:w-1/3">
-      <div className="pb-4">
-        <h1 className="text-center font-medium text-xl">Create Content</h1>
-      </div>
-      <div className="mb-3">
-        <StepWizard step={currentStep} />
-      </div>
-
-      {
-        currentStep === 1 ?
-          <Step1 /> :
-          currentStep === 2 ?
-            <Step2 /> :
-            currentStep === 3 ?
-              <Step3 /> :
-              currentStep === 4 ?
-                <Step4 /> : <></>
-      }
-
-      <div className="mt-6 flex justify-between items-center">
-        <div>
-          {currentStep === 1 ?
-            <SecondaryButton onClick={() => handleClose()}>
-              <>Cancel</>
-            </SecondaryButton> : <SecondaryButton onClick={() => handleStep(DEC)}>
-              <>Back</>
-            </SecondaryButton>
-          }
-
+    <PrimaryModal
+      open={open}
+      handleClose={handleClose}
+      modalClass="w-[380px] sm:w-[500px] md:w-1/2 lg:w-2/5 xl:w-1/3"
+    >
+      <div className="flex flex-col h-full">
+        <div className="pb-4">
+          <h1 className="text-center font-medium text-xl">Create Content</h1>
         </div>
-        <div>
-          <PrimaryButton onClick={() => handleStep(INC)}>
-            <span>Next</span>
-            <ChevronRightIcon className="!text-white" />
-          </PrimaryButton>
+        <div className="mb-3">
+          <StepWizard step={currentStep} />
+        </div>
+
+        {currentStep === 1 ? (
+          <Step1 />
+        ) : currentStep === 2 ? (
+          <Step2 />
+        ) : currentStep === 3 ? (
+          <Step3 />
+        ) : currentStep === 4 ? (
+          <Step4 />
+        ) : (
+          <></>
+        )}
+
+        <div className="flex justify-between items-center mt-auto">
+          <div>
+            {currentStep === 1 ? (
+              <SecondaryButton onClick={() => handleClose()}>
+                <>Cancel</>
+              </SecondaryButton>
+            ) : (
+              <SecondaryButton onClick={() => handleStep(DEC)}>
+                <>Back</>
+              </SecondaryButton>
+            )}
+          </div>
+          <div>
+            <PrimaryButton onClick={() => handleStep(INC)}>
+              <span>Next</span>
+              <ChevronRightIcon className="!text-white" />
+            </PrimaryButton>
+          </div>
         </div>
       </div>
-
     </PrimaryModal>
   );
 };
