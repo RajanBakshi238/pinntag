73815107f554c7ee9eb3d356837a61021788c6bc
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-02-09 22:48:48 +0530

    facebook login button done

--- src/component/businessDetailsScreen/SocialAuth/FacebookLoginButton.jsx
+++ src/component/businessDetailsScreen/SocialAuth/FacebookLoginButton.jsx
@@ -1,25 +1,66 @@
-// FacebookLoginButton.js
-import React from "react";
-// import FacebookLogin from "react-facebook-login";
-import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
-const FacebookLoginButton = ({ onLogin }) => {
+import React, { useEffect, useState } from "react";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+
+const FacebookLoginButton = ({ accessToken, setAccessToken }) => {
+  const [fbCredential, setFbCredential] = useState();
+
   const responseFacebook = (response) => {
     if (response.accessToken) {
-      //   onLogin(response);
-
+      setAccessToken(response.accessToken);
+      setFbCredential(response);
     }
   };
 
+  const handleLogout = () => {
+    setFbCredential(null);
+    setAccessToken("");
+    console.log(window.FB);
+    if (window.FB) {
+      window.FB.logout(function (response) {
+        // user is now logged out
+        console.log(response, ">>>>> response logout");
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (window.FB) {
+      window.FB.getLoginStatus(function (response) {
+        console.log(response, ">>>>>> response")
+        setFbCredential(response?.authResponse);
+      });
+    }
+  }, []);
   return (
-    <FacebookLogin
-      appId="403498615515373"
-      autoLoad={false}
-      fields="name,email,picture,accounts" // Include 'accounts' to get information about user's Pages
-      callback={responseFacebook}
-      render={(renderProps) => (
-        <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={renderProps.onClick}>Connect with facebook</button>
+    <>
+      {fbCredential && accessToken ? (
+        <>
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleLogout}
+          >
+            Connected
+          </button>
+        </>
+      ) : (
+        <FacebookLogin
+          appId="403498615515373"
+          autoLoad={false}
+          fields="name,email,picture,accounts" // Include 'accounts' to get information about user's Pages
+          callback={responseFacebook}
+          render={(renderProps) => (
+            <button
+              type="button"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={renderProps.onClick}
+            >
+              Connect with facebook
+            </button>
+          )}
+        />
       )}
-    />
+    </>
   );
 };
 
